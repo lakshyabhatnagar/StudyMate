e226bd6b52c382887ef87415fb6089bbd929e919
lakshyabhatnagar <lakshyabhatnagar1@gmail.com>
2026-02-04 01:24:35 +0530

    Added README.md & deleted test files/routes

--- config/openai.ts
+++
@@ -1,8 +0,0 @@
-import OpenAI from "openai";
-
-export const client = new OpenAI({
-  apiKey: process.env.DEEPSEEK_API_KEY!,
-  baseURL: "https://api.deepseek.com",
-});
-
-
--- config/openrouter.ts
+++
@@ -1,6 +0,0 @@
-import { OpenRouter } from '@openrouter/sdk';
-const openRouter = new OpenRouter({
-  apiKey: process.env.OPENROUTER_API_KEY!,
-});
-
-export default openRouter;
